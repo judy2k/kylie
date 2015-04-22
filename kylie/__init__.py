fe0324cca346269497c91c50c1cfc5a5ccfecc83
Mark Smith <mark.smith@practicalpoetry.co.uk>
2015-04-22 19:10:00 +0100

    Bump version: 0.1.1 → 0.2.0

--- kylie/__init__.py
+++ kylie/__init__.py
@@ -8,6 +8,6 @@ __all__ = ['Attribute', 'Relation', 'Model']
 
 __author__ = 'Mark Smith'
 __email__ = 'mark.smith@practicalpoetry.co.uk'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 from .kylie import Attribute, Relation, Model
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='kylie',
-    version='0.1.1',
+    version='0.2.0',
     description="Kylie provides mappings between JSON data structures and Python objects.",
     long_description=readme + '\n\n' + history,
     author="Mark Smith",
