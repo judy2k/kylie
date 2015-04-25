0b034230ca297b55bdf8c732264f6d9102a02810
Mark Smith <mark.smith@practicalpoetry.co.uk>
2015-04-25 11:43:38 +0100

    Minor quality improvements.

--- kylie/__init__.py
+++ kylie/__init__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
-"""
-kylie - A module for mapping between JSON and Python classes.
-"""
+"""kylie - A module for mapping between JSON and Python classes."""
 
-__all__ = ['Attribute', 'Relation', 'Model']
+__all__ = ('Attribute', 'Relation', 'Model',)
 
 __author__ = 'Mark Smith'
 __email__ = 'mark.smith@practicalpoetry.co.uk'
