71365fcc78d6fd3a52d2dbc4c40beeda5fe2d9d3
Mark Smith <mark.smith@practicalpoetry.co.uk>
2015-05-05 16:15:27 -0400

    Promotion of AttributeSwitcher to public interface.

--- kylie/__init__.py
+++ kylie/__init__.py
@@ -2,10 +2,16 @@
 
 """kylie - A module for mapping between JSON and Python classes."""
 
-__all__ = ('Attribute', 'Relation', 'Model',)
+__all__ = ('Attribute', 'Relation', 'Model', 'AttributeSwitcher')
 
 __author__ = 'Mark Smith'
 __email__ = 'mark.smith@practicalpoetry.co.uk'
 __version__ = '0.2.0'
 
-from .kylie import Attribute, Relation, Model
+from .kylie import (
+    Attribute,
+    AttributeSwitcher,
+    DeserializationError,
+    Model,
+    Relation,
+)
